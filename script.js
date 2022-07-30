b0381362391c18286ac52832aa189f8697a4923b
Jordan0105 <102000003+Jordan0105@users.noreply.github.com>
2022-07-29 23:05:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,6 @@ function addBorder() {
 const signInBtn = document.getElementsByClassName("btnSignIn");
 
 for(i = 0; i < signInBtn.length; i++){
-  console.log(i);
   signInBtn[i].addEventListener("click", function(){
     window.open("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&", "_self");
   })
@@ -419,7 +418,6 @@ function setOriginal2() {
     } else {
 
       clearTimeout(timeOut);
-      console.log(id2);
       clearInterval(closeTimer);
     }
 
